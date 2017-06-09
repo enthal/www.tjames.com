bb16c9828bfa0f50281f4c211422b9cdcc290b30
Tim James <tjames@tjames.com>
2017-06-09 14:46:51 -0700

    PostCSS!        (And rimraf)

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,34 @@
 const gulp = require('gulp');
+const sequence = require('gulp-sequence');
 const temple = require('temple-cms').gulp;
 
-gulp.task('default', [ 'static', 'template' ]);   // TODO: rimraf?
+gulp.task('default', sequence('rimraf', 'static', ['template', 'css']));
 
 gulp.task('template', () =>
   gulp.src('./templates/*.html')
     .pipe(temple('./content.yml'))
-      .on('error', console.log)
     .pipe(gulp.dest('out'))
 );
+
+gulp.task('css', () => {
+  const postcss    = require('gulp-postcss');
+  const sourcemaps = require('gulp-sourcemaps');
+
+  return gulp.src('style/**/*.css')
+    .pipe( sourcemaps.init() )
+    .pipe( postcss([
+      require('precss'),
+      require('autoprefixer'),
+      ]) )
+    .pipe( sourcemaps.write('.') )
+    .pipe( gulp.dest('out/') );
+});
 
 gulp.task('static', () =>
   gulp.src('./static/**')
     .pipe(gulp.dest('out'))
+);
+
+gulp.task('rimraf', (cb) =>
+  require('rimraf')('./out', cb)
 );
--- scripts/watch.sh
+++ scripts/watch.sh
@@ -2,8 +2,10 @@ nodemon \
  -w ../temple/index.js  \
  -e js,yml,yaml,html,css \
  -w static/ \
+ -w style/ \
  -w content.yml \
  -w templates/  \
+ -w gulpfile.js \
  -x gulp ''
 
  # ../temple/index.js \
